46e994812f7df73aa39945cf6514fa0363e6431d
Savita Pal <savita.pal08@gmail.com>
2022-10-10 14:01:20 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -24,19 +24,19 @@ resource "google_project_service_identity" "sa_identity" {
 }
 
 # Creation of key_ring
-# resource "google_kms_key_ring" "example-keyring" {
-#   name     = "keyring-example1031"
-#   location = "us-central1"
-#   depends_on = [
-#     google_project_service.apis
-#   ]
-# }
+resource "google_kms_key_ring" "example-keyring" {
+   name     = "wf-us-prod-kms-kring-app01"
+   location = var.region
+   depends_on = [
+     google_project_service.apis
+   ]
+}
 
 # Create a KMS key within the provided KMS key-ring
-# resource "google_kms_crypto_key" "key" {
-#   name     = "key1031"
-#   key_ring = google_kms_key_ring.example-keyring.id
-# }
+ resource "google_kms_crypto_key" "key" {
+   name     = "wf-us-prod-kms-key-app01"
+   key_ring = google_kms_key_ring.example-keyring.id
+}
 
 # GAR Repository Resource with CMEK
 resource "google_artifact_registry_repository" "my-repo" {
@@ -44,18 +44,19 @@ resource "google_artifact_registry_repository" "my-repo" {
   repository_id = "wf-us-prod-gar-fghi-app01"
   description   = "example docker repository with cmek"
   format        = "DOCKER"
-  # kms_key_name  = google_kms_crypto_key.key.id
-  #   depends_on = [
-  #     google_kms_crypto_key_iam_member.crypto_key
-  #   ]
+  labels        = var.labels
+  kms_key_name  = google_kms_crypto_key.key.id
+    depends_on = [
+      google_kms_crypto_key_iam_member.crypto_key
+  ]
 }
 
 # Grant cryptoKeyEncrypterDecrypter role to Artifact Registry Service Agent
-# resource "google_kms_crypto_key_iam_member" "crypto_key" {
-#   crypto_key_id = google_kms_crypto_key.key.id
-#   role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
-#   member        = "serviceAccount:${google_project_service_identity.sa_identity.email}"
-# }
+resource "google_kms_crypto_key_iam_member" "crypto_key" {
+  crypto_key_id = google_kms_crypto_key.key.id
+  role          = "roles/cloudkms.cryptoKeyEncrypterDecrypter"
+  member        = "serviceAccount:${google_project_service_identity.sa_identity.email}"
+}
 
 # Granting Artifact Registry repoAdmin role to Application Service Account
 resource "google_artifact_registry_repository_iam_member" "repo" {
