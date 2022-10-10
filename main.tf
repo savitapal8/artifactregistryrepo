7ec9974be97e79b32fc7bd2ab115297eeaaaa462
Savita Pal <savita.pal08@gmail.com>
2022-10-10 17:04:12 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -45,11 +45,11 @@ resource "google_artifact_registry_repository" "my-repo" {
   repository_id = "wf-us-prod-gra-fghi-app01"
   description   = "example docker repository with cmek"
   format        = "DOCKER"
-  #labels        = var.labels
-  kms_key_name  = google_kms_crypto_key.key.id
-    depends_on = [
-      google_kms_crypto_key_iam_member.crypto_key
-  ]
+  labels        = var.labels
+  #kms_key_name  = google_kms_crypto_key.key.id
+  #  depends_on = [
+  #    google_kms_crypto_key_iam_member.crypto_key
+  #]
 }
 
 # Grant cryptoKeyEncrypterDecrypter role to Artifact Registry Service Agent
