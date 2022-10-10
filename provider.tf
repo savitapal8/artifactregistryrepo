39ac7585de227e937a0e9a6fc0813ae143cb2d72
Savita Pal <savita.pal08@gmail.com>
2022-10-10 13:50:01 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,11 +1,11 @@
 provider "google" {
   project = var.project_id
-  region  = "us-central1"
+  region  = var.region
   access_token = var.access_token
 }
 
 provider "google-beta" {
   project = var.project_id
-  region  = "us-central1"
+  region  = var.region
   access_token = var.access_token
 }
